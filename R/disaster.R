df6d74ff3de8be1d41737c52a43ffdef7d7b4f76
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-10-31 18:44:38 +0900

    start 참사 데이터 ... 마무리 못함

--- R/disaster.R
+++ R/disaster.R
@@ -77,5 +77,44 @@ nature_tbl <- nature_raw %>%
 
 nature_tbl
 
+## 2.4. 해럴드경제 뉴스(2014) -------------
+# https://www.mois.go.kr/frt/bbs/type001/commonSelectBoardArticle.do?bbsId=BBSMSTR_000000000014&nttId=89542
 
+herald_raw <- read_lines("data-raw/herald_news_disaster.txt")
+
+herald_tbl <- herald_raw %>%
+  as_tibble() %>%
+  slice(2:n()) %>%
+  mutate(날짜 = str_extract(value,
+                          pattern = "\\d{4}년\\s?\\d{1,2}월\\s?\\d{1,2}일") %>%
+           lubridate::ymd(.)) %>%
+  mutate(사망 = str_extract(value, pattern = "\\d{1,4}명$") %>%
+           parse_number(.)) %>%
+  mutate(참사내역 = str_remove(value, pattern = "\\d{1,4}명$") %>%
+           str_remove(pattern = "\\d{4}년\\s?\\d{1,2}월\\s?\\d{1,2}일") %>%
+           str_trim(.)) %>%
+  select(날짜, 사망, 참사내역)
+
+herald_tbl
+
+## 2.5. 태풍 인명피해(나무위키) -------------
+# https://namu.wiki/w/%ED%83%9C%ED%92%8D/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD/%EA%B0%81%EC%A2%85%20%EA%B8%B0%EB%A1%9D%20%EC%88%9C%EC%9C%84
+
+typoon_raw <- read_html("https://namu.wiki/w/%ED%83%9C%ED%92%8D/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD/%EA%B0%81%EC%A2%85%20%EA%B8%B0%EB%A1%9D%20%EC%88%9C%EC%9C%84",
+                        encoding = 'utf-8') %>%
+  html_elements('table')
+
+typoon_tbl <- typoon_raw %>%
+  html_table() %>%
+  .[[1]] %>%
+  slice(3:n()) %>%
+  mutate(시작일 = lubridate::ymd(X2),
+         종료일 = lubridate::ymd(X3),
+         사망   = parse_number(X5)) %>%
+  select(시작일, 종료일, 사망, 태풍명 = X4) %>%
+  mutate(기간 = lubridate::interval(시작일, 종료일)) %>%
+  mutate(지속일수 = lubridate::as.duration(기간) %>% as.numeric("days")) %>%
+  select(-기간)
+
+typoon_tbl
 
